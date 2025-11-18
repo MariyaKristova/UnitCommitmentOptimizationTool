2e33ee9d9b598449403fb951807ed765382477f0
MariyaKristova <maria.vinogradova@gmail.com>
2025-11-18 13:15:40 +0200

    update custom errors pages

--- forwardcurve/settings.py
+++ forwardcurve/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- forwardcurve/urls.py
+++ forwardcurve/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
+from plotapp import views
 
+handler404 = views.custom_404 # noqa: F811
+handler500 = views.custom_500 # noqa: F811
 
 urlpatterns = [
     path('admin/', admin.site.urls),
